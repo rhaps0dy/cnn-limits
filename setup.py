044e12fc1ab8675e8f0b3a293bf495e18b0ed37c
Adria Garriga-Alonso <ag919@login-e-3.data.cluster>
2020-04-01 01:22:41 +0100

    fixed setup.py; working on slurm

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(
     license="Apache License 2.0",
     packages=["cnn_limits"],
     install_requires=[
-        "cnn-gp @ git+git@github.com:rhaps0dy/cnn-gp.git@b61bc01f8e2dfd2427798793a3677c5b1a6a706c#egg=cnn_gp",
+        "cnn-gp @ git+ssh://git@github.com/cambridge-mlg/cnn-gp.git@b61bc01f8e2dfd2427798793a3677c5b1a6a706c",
         "jax>=0.1.59<0.2",
-        "neural-tangents @ git+git@github.com:rhaps0dy/neural-tangents.git@2020cae57a2d09c7a18d9b61299d1f2a6d1968ed#egg=neural_tangents",
-        "nigp @ git+git@github.com:cambridge-mlg/time-gp.git@6ac4f3fc8789d9fb6c9f94115810a445b61e3aeb#egg=nigp",
+        "neural-tangents @ git+ssh://git@github.com/rhaps0dy/neural-tangents.git@2020cae57a2d09c7a18d9b61299d1f2a6d1968ed",
+        "nigp @ git+ssh://git@github.com/cambridge-mlg/nigp.git@6ac4f3fc8789d9fb6c9f94115810a445b61e3aeb",
         "jaxlib @ https://storage.googleapis.com/jax-releases/cuda101/jaxlib-0.1.42-cp37-none-linux_x86_64.whl",
         "jax>=0.1.59"
         "gpytorch>=1.0.1<1.1",
